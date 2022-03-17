0352e91c1cd579f666d87c5f25ddbd7763642ce4
Fabian <Fabian.Kiesner@gmx.de>
2022-03-17 21:19:19 +0100

    add: exception handling

--- arithmetic/devisor/TestDevisor.java
+++ arithmetic/devisor/TestDevisor.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class TestDevisor {
@@ -9,11 +11,46 @@ public class TestDevisor {
         System.out.println("Wie soll die erste Zahl lauten?");
         System.out.println("Es sind nur ganze Zahlen im Integer Zahlenbereich erlaubt!");
         System.out.print("Wert: ");
-        number1=input.nextInt();
+        while (true) {//TODO Infinty Loop with IMExc
+            try {if(input.hasNextInt()){
+                    number1 = input.nextInt();
+                }
+                break;
+            } catch (InputMismatchException IMExc) {
+                System.out.println("Die eingegebene Zahl ist kein Integer oder ist außerhalb des 32 Bit Bereichs.");
+                System.out.println(
+                        "Deine Zahl sollte eine ganze Zahl sein und zwischen " + Integer.toString(Integer.MIN_VALUE)
+                                + " sowie " + Integer.toString(Integer.MAX_VALUE) + " liegen.");
+                System.out.println("Versuche es noch einmal!");
+            } catch (NoSuchElementException NSEExc) {
+                System.out.println("Es fand keine Eingabe statt!");
+            } catch (IllegalStateException ISExc) {
+                input = new Scanner(System.in);
+            }
+        }
         System.out.println("Wie soll die zweite Zahl lauten?");
         System.out.println("Es sind wieder nur ganze Zahlen im Integer Zahlenbereich erlaubt!");
         System.out.print("Wert: ");
-        number2=input.nextInt();
-        System.out.println("Der größte gemeinsame Teiler von: "+Integer.toString(number1)+" und "+Integer.toString(number2)+" lautet: "+Integer.toString(Devisor.getGreatestCommundDeviderEuclid(number1, number2)));
+        while (true) {
+            try {
+                number2 = input.nextInt();
+                break;
+            } catch (InputMismatchException IMExc) {
+                System.out.println("Die eingegebene Zahl ist kein Integer oder ist außerhalb des 32 Bit Bereichs.");
+                System.out.println(
+                        "Deine Zahl sollte eine ganze Zhal sein und zwischen " + Integer.toString(Integer.MIN_VALUE)
+                                + " sowie " + Integer.toString(Integer.MAX_VALUE) + " liegen.");
+                System.out.println("Versuche es noch einmal!");
+            } catch (NoSuchElementException NSEExc) {
+                System.out.println("Es fand keine Eingabe statt!");
+            } catch (IllegalStateException ISExc) {
+                input = new Scanner(System.in);
+            }
+
+        }
+        System.out.println(
+                "Der größte gemeinsame Teiler von: " + Integer.toString(number1) + " und " + Integer.toString(number2)
+                        + " lautet: " + Integer.toString(Devisor.getGreatestCommundDeviderEuclid(number1, number2)));
+        input.close();
     }
 }
